a49eaffbed94a6e226ead2872ef597bb01eed08b
caradrian14 <adria.jorda.m@gmail.com>
2026-01-07 10:46:51 +0100

    [add] doc swagger & openApiInfoController

---
+++ app/Http/Controllers/OpenApiInfoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *     title="API de Pedidos (Orders)",
+ *     version="1.0.0",
+ *     description="API para gestionar pedidos en Laravel",
+ *     @OA\Contact(
+ *         email="adria@example.com",
+ *         name="Adrià Jordà Martínez"
+ *     )
+ * )
+ */
+class OpenApiInfoController extends Controller
+{
+    // Este controlador solo existe para documentar la API.
+}
