a73646e00f90323d34db1e04ea9321d8cf22e013
Adria Jorda <ajm@scanspan.eu>
2026-01-08 17:01:50 +0100

    [add] general functions to Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $order = Product::create($request->all());
+        return response()->json($order, 201);
+    }
+
+    public function show(string $id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $order = Product::findOrFail($id);
+        $order->update($request->all());
+        return response()->json($order, 200);
+    }
+
+    public function destroy(string $id)
+    {
+        Product::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
 }
