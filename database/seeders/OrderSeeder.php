95a3a9ab5b457393630803509100834716c63fe4
Adria Jorda <ajm@scanspan.eu>
2026-01-09 15:27:28 +0100

    [add] Seeders

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,29 @@ class OrderSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $users = User::all();
+        $products = Product::all();
+
+        // Crear 15 pedidos
+        Order::factory()->count(15)->create()->each(function ($order) use ($products) {
+            // Asociar entre 1 y 5 productos aleatorios a cada pedido
+            $randomProducts = $products->random(rand(1, 5));
+
+            foreach ($randomProducts as $product) {
+                $quantity = rand(1, 3);
+                $order->products()->attach($product, [
+                    'quantity' => $quantity,
+                    'unit_price' => $product->price,
+                ]);
+            }
+
+            // Actualizar el total del pedido
+            $total = $order->products->sum(function ($product) use ($order) {
+                return $product->pivot->quantity * $product->pivot->unit_price;
+            });
+            $order->update(['total_amount' => $total]);
+        });
     }
 }
