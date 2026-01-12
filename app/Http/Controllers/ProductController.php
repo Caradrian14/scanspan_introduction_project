a71b61b645b2246bcb476b36154295cc17091a4b
Adria Jorda <ajm@scanspan.eu>
2026-01-12 12:25:16 +0100

    [doc] eloquent examples

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,6 +20,22 @@ class ProductController extends Controller
     public function show(string $id)
     {
         return Product::findOrFail($id);
+        // Obtener productos con precio mayor a 50, ordenados por nombre
+            // $products = Product::where('price', '>', 50)
+            //        ->orderBy('name')
+            //        ->get();
+            // Obtener productos con sus categorías (relación muchos a muchos)
+        // $products = Product::with('categories')->get();
+
+        //Ejemplo mas complejo
+        $customer = Customer::with(['orders.products'])->find(1);
+
+    // foreach ($customer->orders as $order) {
+    //     echo "Pedido #" . $order->id . ":<br>";
+    //     foreach ($order->products as $product) {
+    //         echo "- " . $product->name . " (Precio: " . $product->price . ")<br>";
+    //     }
+    // }
     }
 
     public function update(Request $request, string $id)
@@ -33,5 +49,7 @@ class ProductController extends Controller
     {
         Product::findOrFail($id)->delete();
         return response()->json(null, 204);
+        // Ejemplo distinto: eliminar todos los productos con precio menor a 10
+        // Product::where('price', '<', 10)->delete();
     }
 }
